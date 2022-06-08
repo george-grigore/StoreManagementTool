46f5c6bc532df5a56280e51f32ca024bf729d166
george-grigore <grigoregeorge631980@yahoo.co.uk>
2022-06-08 23:47:20 +0300

    add Item entity

---
+++ src/main/java/com/store/entity/Item.java
@@ -0,0 +1,25 @@
+package com.store.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String itemName;
+    private double itemPrice;
+
+}
